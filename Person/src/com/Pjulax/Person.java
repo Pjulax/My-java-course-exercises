9fc9cd1b0a022bb971af4fab879d6ceb39a13356
Pjulax <pawelpo99@gmail.com>
2019-03-29 17:11:52 +0100

    Person class

---
+++ Person/src/com/Pjulax/Person.java
@@ -0,0 +1,60 @@
+package com.Pjulax;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age){
+        if(age >= 0 && age <= 100) {
+            this.age = age;
+        } else{
+            this.age = 0;
+        }
+    }
+
+    public boolean isTeen(){
+        if( this.age > 12 && this.age < 20 ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getFullName(){
+        if(this.firstName.isEmpty()){
+            if(this.lastName.isEmpty()){
+                return "";
+            }else {
+                return lastName;
+            }
+        } else {
+            if( this.lastName.isEmpty()){
+                return firstName;
+            }else {
+                return firstName + " " + lastName;
+            }
+        }
+    }
+
+}
